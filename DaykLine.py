97ed27a8e3cfcbb29af500d3f9f79186c3b7ec85
shayne007 <shayne007@sina.cn>
2025-05-28 19:20:03 +0800

    [refactor] explain the code

--- DaykLine.py
+++ DaykLine.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import akshare as ak
 
-df = ak.stock_zh_a_hist(symbol="300750", 
-                                        period="daily", 
-                                        start_date="20250407", 
-                                        end_date='20250411', 
-                                        adjust="qfq")
+# stock symbol of "宁德时代" is "300750"
+stock_symbol = "300750"
+
+# we fetch daily history data between start and end date
+# adjust 默认返回不复权的数据; qfq: 返回前复权后的数据; hfq: 返回后复权后的数据
+df = ak.stock_zh_a_hist(symbol=stock_symbol,
+                        period="daily",
+                        start_date="20250501",
+                        end_date='20250520',
+                        adjust="qfq")
 
 print(df.dtypes)
+
+# transfer the date format from object to datetime64
 df['日期'] = pd.to_datetime(df['日期'])
 print(df.dtypes)
 
 df.set_index('日期', inplace=True)
 df.sort_index(ascending=False, inplace=True)
 
-df.to_csv('300750.csv')
+df.to_csv('%s.csv' % stock_symbol)
